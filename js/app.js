e24905cba8de0bb7ac5d14e809efc08ba75603d6
Saynka <sankakale@gmail.com>
2020-10-05 14:03:05 -0700

    dropdownworking

--- js/app.js
+++ js/app.js
@@ -1,56 +1,60 @@
 'use strict';
 
+const animalsArray = [];
 $().ready(() => {
-  const animalsArray = [];
   $.ajax('data/page-1.json', { method: 'GET', dataType: 'JSON' }).then((data) => {
     data.forEach(hornedAnimal => {
       var newAnimal = new Animal(hornedAnimal);
       newAnimal.createHTML();
-
     });
+    dropDown();
 
   });
-  function Animal(object) {
-    this.title = object.title;
-    this.description = object.description;
-    this.keyword = object.keyword;
-    this.horns = object.horns;
-    this.image_url = object.image_url;
-    animalsArray.push(this);
-  };
+});
+function Animal(object) {
+  this.title = object.title;
+  this.description = object.description;
+  this.keyword = object.keyword;
+  this.horns = object.horns;
+  this.image_url = object.image_url;
+  animalsArray.push(this);
+};
 
 
-  Animal.prototype.createHTML = function () {
-    let myTemplate = $('#template').html();
-    let html = Mustache.render(myTemplate, this);
+Animal.prototype.createHTML = function () {
+  let myTemplate = $('#template').html();
+  let html = Mustache.render(myTemplate, this);
 
-    $('main').append(html);
-  };
+  $('main').append(html);
+};
 
-  function dropDown() {
+function dropDown() {
 
-    let keywords = [];
-    //grab drop down ID
+  let keywords = [];
+  //grab drop down ID
 
-    animalsArray.forEach((Animal) => {
-      let nameOfKey = Animal.keyword;
-      // ! - does the opposite
-      if (!keywords.includes(nameOfKey)) {
-        keywords.push(nameOfKey);
+  animalsArray.forEach((Animal) => {
+    let nameOfKey = Animal.keyword;
+    // ! - does the opposite
+    if (!keywords.includes(nameOfKey)) {
+      keywords.push(nameOfKey);
+    }
+  })
+  keywords.forEach(keyword => {
+    let option = $(`<option value=${keyword}>${keyword}</option>`)
+    $('#dropDown').append(option)
+    
+  })
+  console.log(keywords);
+  // .on( events [, selector ] [, data ] )
+
+  // add event listener to dropdown
+  $('#dropDown').on('change', function () {
+    $('.Animal').remove();
+    animalsArray.forEach(Animal => {
+      if (Animal.keyword === this.value) {
+        Animal.myTemplate();
       }
     })
-
-    // .on( events [, selector ] [, data ] )
-
-    // add event listener to dropdown
-    $('#dropDown').on('change', function () {
-      $('.animal').remove();
-      animalsArray.forEach(Animal => {
-        if (Animal.keyword === this.value) {
-          Animal.template();
-        }
-      })
-    })
-  }
-
-});
+  })
+}
